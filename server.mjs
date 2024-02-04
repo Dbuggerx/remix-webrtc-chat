c9364ac879f60bbc78dc61a2f95ae8dc9c197cf3
Danilo Cestari <danilofcestari@gmail.com>
2024-02-04 15:16:20 +0100

    feat: setup peerjs server

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import { createRequestHandler } from "@remix-run/express";
 import { broadcastDevReady } from "@remix-run/node";
 import express from "express";
+import { ExpressPeerServer } from "peer";
 
 // notice that the result of `remix build` is "just a module"
 import * as build from "./build/index.js";
@@ -8,12 +9,19 @@ import * as build from "./build/index.js";
 const app = express();
 app.use(express.static("public"));
 
-// and your app is "just a request handler"
-app.all("*", createRequestHandler({ build }));
-
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   if (process.env.NODE_ENV === "development") {
     broadcastDevReady(build);
   }
   console.log("App listening on http://localhost:3000");
 });
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: "/myapp",
+});
+
+app.use("/peerjs", peerServer);
+
+// and your app is "just a request handler"
+app.all("*", createRequestHandler({ build }));
